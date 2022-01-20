c66b8e6c71b320a200fbffe9036b4810374470d2
khanumair1650 <khanumair1650@gmail.com>
2022-01-20 13:48:43 +0530

    new commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 app.get("/", (req, res) => {
-res.send("Hello Heroku");
+res.send("TCET,Hello Heroku");
 })
 app.listen(process.env.PORT || port, () => {
 console.log("listening 8080...");
