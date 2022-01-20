e400ab706fbbec128556a609689f54962e1c2252
khanumair1650 <khanumair1650@gmail.com>
2022-01-20 15:24:10 +0530

    last committtttt

--- users_module.js
+++ users_module.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 //mongodb://127.0.0.1:27017/dbname
 //const conn_str = "mongodb://localhost:27017/tcet";
-const conn_str = "mongodb://<admin123>:<admin123>@cluster0-shard-00-00.dslyw.mongodb.net:27017,cluster0-shard-00-01.dslyw.mongodb.net:27017,cluster0-shard-00-02.dslyw.mongodb.net:27017/<tcetapp5>?ssl=true&replicaSet=atlas-3xk2hf-shard-0&authSource=admin&retryWrites=true&w=majority";
+const conn_str = "mongodb+srv://admin123:admin123@users.3ak2z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose.connect(conn_str, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log("Connected successfully..."))
 .catch( (error) => console.log(error) );
